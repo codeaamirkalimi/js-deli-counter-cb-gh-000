4b0fc2936c8b710b9b7ad8b0ee064753b755fde3
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-03-14 10:26:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ function takeANumber(lineNo, personName){
   return `Welcome, ${personName} You are number ${lineNo} in line.`;
 }
 function nowServing(katzDeliLine){
-
+  if(katzDeliLine.lenth <= 0){
+    return "There is nobody waiting to be served!";
+  }
 }
